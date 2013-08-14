32ad9ffc22fb550e540f439f63d2a02f7991867c
Loz Calver <lozcalver@bigfork.co.uk>
2013-08-14 16:36:44 +0100

    Update _config.php options
    New regexp for matching more dev addresses, remove a few lines that don't do anything anymore

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,6 +1,6 @@
 <?php
 // optionally use a different database on dev
-if (preg_match('/\.local$/', $_SERVER['HTTP_HOST'])) {
+if (preg_match('/(\.local|\.dev|\.proxylocal|192\.168)$/', $_SERVER['HTTP_HOST'])) {
 	define('SS_DATABASE_SERVER', 'localhost');
 } else {
 	define('SS_DATABASE_SERVER', 'localhost');
@@ -17,14 +17,8 @@ $database = '';
 // load config from environment
 require_once("conf/ConfigureFromEnv.php");
 
-// set mysql charset
-MySQLDatabase::set_connection_charset('utf8');
-
 // Set the site locale
 i18n::set_locale('en_GB');
-
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
 // Set default password - remove this after creating a proper user
 Security::setDefaultAdmin('admin','password');
@@ -43,5 +37,3 @@ HtmlEditorConfig::get('cms')->setButtonsForLine(1, "formatselect,separator,bulli
 	."separator,spellchecker,undo,redo,code");
 HtmlEditorConfig::get('cms')->setButtonsForLine(2, "tablecontrols");
 HtmlEditorConfig::get('cms')->setButtonsForLine(3, '');
-
-// Extensions
