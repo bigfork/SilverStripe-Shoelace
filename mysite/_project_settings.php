0de9f2222117947d4223e3ade72f1b99164ce979
Colin Richardson <feejin@gmail.com>
2013-09-04 22:10:20 +0100

    Separate vars required for install into project settings file

---
+++ mysite/_project_settings.php
@@ -0,0 +1,14 @@
+<?php
+// project name
+global $project;
+$project = 'mysite';
+
+// theme name
+$theme = 'testing';
+
+// database name
+global $database;
+$database = $theme;
+
+// databast host
+$localhost = 'feejin.local';
---
+++ securitytemplates/_config.php
@@ -0,0 +1,4 @@
+<?php
+
+// Comment this line out to use the default SilverStripe logo
+LeftAndMain::require_css('SecurityTemplates/css/branding.css');
