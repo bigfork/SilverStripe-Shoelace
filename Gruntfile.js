2ec89af240ee646b8c31d94dd6287916779aa44a
Loz Calver <lozcalver@bigfork.co.uk>
2014-09-10 11:14:23 +0100

    Move media queries to variables, add CSSC

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,6 +150,25 @@ module.exports = function(grunt) {
 			}
 		},
 
+		// csscompress
+		cssc: {
+			main: {
+				files: {
+					'themes/<%= pkg.name %>/css/style.css': 'themes/<%= pkg.name %>/css/style.css'
+				},
+				options: {
+					sortSelectors: false,
+					sortDeclarations: false,
+					consolidateViaDeclarations: false,
+					consolidateViaSelectors: false,
+					consolidateMediaQueries: true,
+					compress: true,
+					sort: false,
+					safe: false
+				}
+			}
+		},
+
 		// add vendor prefixes
 		autoprefixer: {
 			dist: {
@@ -194,7 +213,7 @@ module.exports = function(grunt) {
 
 	// Task(s).
 	grunt.registerTask('js',  ['jshint', 'uglify']);
-	grunt.registerTask('css',  ['scsslint', 'sass', 'autoprefixer']);
+	grunt.registerTask('css',  ['scsslint', 'sass', 'autoprefixer', 'cssc']);
 	grunt.registerTask('png',  ['tinypng']);
 	grunt.registerTask('default', ['js', 'css']);
 	grunt.registerTask('push', function(type, dir, deploy, db) {
