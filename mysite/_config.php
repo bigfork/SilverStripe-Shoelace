a69cf591bb7c4f2934f32e9909d89cfbdfc0dfb2
Loz Calver <kinglozzer@gmail.com>
2015-02-03 21:00:01 +0000

    Tidy _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -9,7 +9,7 @@ if( ! preg_match('/(\.local|\.dev|\.proxylocal\.com)$/', $_SERVER['HTTP_HOST']))
 }
 
 // use database name from config.yml unless defined in environment
-if( ! defined('SS_DATABASE_NAME')){
+if( ! defined('SS_DATABASE_NAME')) {
 	define('SS_DATABASE_NAME', $config->get('Database', 'name'));
 }
 
@@ -22,20 +22,15 @@ i18n::set_locale('en_GB');
 // Set default password - remove this after creating a proper user
 Security::setDefaultAdmin('admin', 'password');
 
-// Secure test site with a CMS username & password (environment type must be "test")
-// if(Director::isTest()) BasicAuth::protect_entire_site();
-
-// Send debug errors via email
-// if(Director::isLive()) Debug::send_errors_to("your@email.com");
-
 // TinyMCE Config
-HtmlEditorConfig::get('cms')->disablePlugins('emotions', 'fullscreen');
-HtmlEditorConfig::get('cms')->setButtonsForLine(1, "formatselect,separator,bullist,numlist,
+$config = HtmlEditorConfig::get('cms');
+$config->disablePlugins('emotions', 'fullscreen');
+$config->setButtonsForLine(1, "formatselect,separator,bullist,numlist,
 	separator,bold,italic,sup,sub,separator,sslink,unlink,anchor,separator,ssmedia,pasteword,
 	separator,spellchecker,undo,redo,code");
-HtmlEditorConfig::get('cms')->setButtonsForLine(2, "tablecontrols");
-HtmlEditorConfig::get('cms')->setButtonsForLine(3, '');
-HtmlEditorConfig::get('cms')->setOptions(array(
+$config->setButtonsForLine(2, "tablecontrols");
+$config->setButtonsForLine(3, '');
+$config->setOptions(array(
 	'theme_advanced_blockformats' => 'p,h2,h3',
 	'paste_auto_cleanup_on_paste' => 'true',
 	'paste_remove_styles' => 'true',
