489ba6f64cdb20d8f99c4045c4d13f10dd13d4a4
Stan Hutcheon <stanhutcheon@bigfork.co.uk>
2016-02-12 12:38:19 +0000

    Update GA tracker to support mailto: clicks

--- themes/default/js/src/ga.bigfork.js
+++ themes/default/js/src/ga.bigfork.js
@@ -7,19 +7,20 @@
 	// regex
 	var extensions = ['pdf', 'docx?', 'xlsx?', 'pp(t|s)x?', 'csv', 'rtf'];
 
-	$('a').each(function() {
-		var self = this, pathname = this.pathname || '';
+	$('a[href]').each(function() {
+		var self = this, pathname = this.pathname || '',
+		type = !this.protocol.match(/^mail/) ? 'Link' : 'Email';
 
-		if( !(pathname.match(new RegExp('\.(' + extensions.join('|') + ')$', 'i'))) && this.host === window.location.host ) return;
+		if(type == 'Link' && !(pathname.match(new RegExp('\.(' + extensions.join('|') + ')$', 'i'))) && this.host === window.location.host) return;
 
-		$.each({'action': 'Clicked', 'label': (pathname.replace(/(.*\/)+/,'') || this.innerHTML), 'value': document.title}, function(attr, val) {
-			$(self).attr('data-track', 'link').data(attr, val);
+		$.each({action: 'Clicked', label: (pathname.replace(/(.*\/)+/,'') || this.innerHTML), value: d.title}, function(attr, val) {
+			$(self).attr('data-track', type).data(attr, val);
 		});
 	});
 
 	$(d).on('click', 'a[data-track]', function() {
 		var $this = $(this);
-		ga('send', 'event', 'Link', $this.data('action'), $this.data('value'), 1);
+		(typeof ga !== 'undefined') && ga('send', 'event', $this.data('track'), $this.data('action'), $this.data('value'), 1);
 	});
 
 })(document);
@@ -33,14 +34,14 @@
 (function(d) {
 
 	// regex
-	var extensions = ['pdf', 'docx?', 'xlsx?', 'pp(t|s)x?', 'csv', 'rtf'],
-		a = d.querySelectorAll('a');
+	var extensions = ['pdf', 'docx?', 'xlsx?', 'pp(t|s)x?', 'csv', 'rtf'];
 
-	for(var i = 0; i < a.length; i++) {
+	for(var a = d.querySelectorAll('a[href]'), i = 0; i < a.length; i++) {
 		var self = a[i], pathname = self.pathname || '',
-		attrs = {'action': 'Clicked', 'label': (pathname.replace(/(.*\/)+/,'') || self.innerHTML), 'value': d.title};
+		type = !self.protocol.match(/^mail/) ? 'Link' : 'Email',
+		attrs = {track: type, action: 'Clicked', label: (pathname.replace(/(.*\/)+/,'') || self.innerHTML), value: d.title};
 
-		if( !(pathname.match(new RegExp('\.(' + extensions.join('|') + ')$', 'i'))) && self.host === window.location.host ) continue;
+		if(type == 'Link' && !(pathname.match(new RegExp('\.(' + extensions.join('|') + ')$', 'i'))) && self.host === window.location.host) continue;
 
 		for(var p in attrs) {if(attrs.hasOwnProperty(p)) {
 			self.setAttribute('data-' + p, attrs[p]);
